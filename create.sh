c285e89e5f1d0678ecd3f8c25a313bf9d32ea219
nicholas <nicholas.chamansingh@gmail.com>
2019-03-24 16:56:36 -0400

    updated

--- create.sh
+++ create.sh
@@ -4,5 +4,5 @@ aws cloudformation create-stack \
     --template-body file://single_instance.yml \
     --stack-name single-instance \
     --region us-east-1 \
-    --parameters ParameterKey=KeyName,ParameterValue=week7 \
+    --parameters ParameterKey=KeyName,ParameterValue=#yourpemfile \
       ParameterKey=InstanceType,ParameterValue=t2.micro
